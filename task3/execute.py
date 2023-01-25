287d7ba0f8382c09f6504f8192317fc4669aac6b
Miu-04 <jmiu1829@gmail.com>
2023-01-25 04:14:02 -0500

    updated to match Chulwon design

--- task3/execute.py
+++ task3/execute.py
@@ -4,8 +4,8 @@ import numpy as np
 
 
 ## submit the simulation jobs to cluster
-Lam_us = np.round(np.arange(0.42,1.22+0.05,0.03),2)
-Lam_ss = np.round(np.arange(0.47,1.27+0.05,0.03),2)
+Lam_us = np.round(np.arange(0.75,0.80+0.01,0.01),2)
+Lam_ss = np.round(np.arange(0.72,0.77+0.01,0.01),2)
 for i in range(len(Lam_us)):
     os.system('sbatch task3_automated.sh '+str(Lam_us[i])+' '+str(Lam_ss[i]))
     print('Job for Lam_u = '+str(Lam_us[i])+' has been submitted.')
