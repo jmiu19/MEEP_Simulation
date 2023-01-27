56c8f947235aca3f0900d19a49bd917f5186ab59
Miu-04 <jmiu1829@gmail.com>
2023-01-27 01:28:46 -0500

    added coupled-bands

---
+++ coupled_bands/execute.py
@@ -0,0 +1,11 @@
+import os
+import sys
+import numpy as np
+
+
+## submit the simulation jobs to cluster
+Lam_us = np.round(np.arange(0.74,0.77+0.01,0.01),2)
+Lam_ss = np.round(np.arange(0.69,0.72+0.01,0.01),2)
+for i in range(len(Lam_us)):
+    os.system('sbatch task3_automated.sh '+str(Lam_us[i])+' '+str(Lam_ss[i]))
+    print('Job for Lam_u = '+str(Lam_us[i])+' has been submitted.')
---
+++ coupled_bands/task3_plot.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+import os
+import sys
+from matplotlib import pyplot as plt
+
+
+
+## read in data
+df = pd.read_csv('nanobeam_cavity_varylength.dat', header=None, sep=',', engine='python')
+df.columns = ['lam_up', 'lam_low', 'nu', 'Q-factor']
+lam_ups = df['lam_up'].values.tolist()
+df['lambda'] = np.round(1/df['nu'].values, 8)
+df = np.round(df.sort_values('lam_up', axis=0),8)
+df.to_csv('output/nanobeam_cavity_varylength.csv')
+
+## plot Q-factor over source wavelength (lower bound)
+plt.figure(figsize=(5,3))
+plt.scatter(df['lam_low'], df['Q-factor'], s=55, label="Q-factor", marker="+", color="darkorange")
+plt.plot(df['lam_low'], df['Q-factor'], alpha=0.3, color="blue")
+plt.legend()
+plt.grid()
+plt.xlabel(r"source wavelength (lower bound) [$\mu$m]", fontsize=14)
+plt.ylabel("Q-factor", fontsize=14)
+plt.savefig('output/Q_sCav.png', bbox_inches='tight')
+
+## plot resonant wavelength over source wavelength (lower bound)
+plt.figure(figsize=(5,3))
+plt.scatter(df['lam_low'], df['lambda'], s=55, label="lambda", marker="+", color="darkorange")
+plt.plot(df['lam_low'], df['lambda'], alpha=0.3, color="red")
+plt.legend()
+plt.grid()
+plt.xlabel(r"source wavelength (lower bound) [$\mu$m]", fontsize=14)
+plt.ylabel(r"resonant $\lambda$ ($\mu$m)", fontsize=14)
+plt.savefig('output/lambda_sCav.png', bbox_inches='tight')
+
+
+
+## plot the configuration
+import h5py
+
+eps_h5file = h5py.File('nanobeam-eps-000613.88.h5','r')
+eps_data = np.array(eps_h5file['eps'])
+ey_h5file = h5py.File('nanobeam-ey-000613.88.h5','r')
+ey_data = np.array(ey_h5file['ey'])
+plt.figure(dpi=100)
+plt.imshow(eps_data.transpose(), interpolation='spline36', cmap='binary')
+plt.imshow(ey_data.transpose(), interpolation='spline36', cmap='RdBu', alpha=0.9)
+plt.axis('off')
+plt.savefig('output/config.png', bbox_inches='tight')
